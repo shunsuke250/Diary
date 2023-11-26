d022c3f6c2f7b61482cb904066c7c0feef15d7cb
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-26 13:37:30 +0900

    ✨: TableViewCellに日付と曜日、日記文章を表示する機能を追加

--- Diary/Diary/DiaryTableViewCell.swift
+++ Diary/Diary/DiaryTableViewCell.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class DiaryTableViewCell: UITableViewCell {
+    private lazy var verticalStackView: UIStackView = {
+        $0.axis = .vertical
+        $0.alignment = .center
+        $0.spacing = 0
+        return $0
+    }(UIStackView(arrangedSubviews: [dateLabel, dayLabel]))
+
+    private lazy var parentStackView: UIStackView = {
+        $0.axis = .horizontal
+        $0.alignment = .top
+        $0.spacing = 10
+        return $0
+    }(UIStackView(arrangedSubviews: [verticalStackView, diaryLabel]))
+
     private let dateLabel: UILabel = {
-        $0.font = .systemFont(ofSize: 14.0)
+        $0.font = .systemFont(ofSize: 26.0, weight: .regular)
         $0.textColor = Color.black
-        $0.text = "text"
+        $0.text = "26"
+        return $0
+    }(UILabel())
+
+    private let dayLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 12.0, weight: .regular)
+        $0.textColor = Color.red
+        $0.text = "日曜日"
+        return $0
+    }(UILabel())
+
+    private let diaryLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 16.0, weight: .regular)
+        $0.numberOfLines = 5
+        $0.text = "このように文章を\n改行して入力することができます。\nこのアプリはLifeBearの日記機能のUIを参考にして\n作成されています。"
         return $0
     }(UILabel())
 
@@ -25,10 +53,11 @@ class DiaryTableViewCell: UITableViewCell {
     }
 
     private func setup() {
-        addSubview(dateLabel)
+        contentView.addSubview(parentStackView)
 
-        dateLabel.snp.makeConstraints {
-            $0.left.top.equalToSuperview()
+        parentStackView.snp.makeConstraints {
+            $0.top.left.equalToSuperview().offset(10)
+            $0.bottom.right.equalToSuperview().inset(10)
         }
     }
 }
