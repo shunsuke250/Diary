3f9130e942f422c8dde456d77c8a840d995b719c
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-26 11:31:14 +0900

    ✨: Storyboardを削除したXcodeプロジェクトを作成しSnapKitを導入

---
+++ Diary/Diary/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by 副山俊輔 on 2023/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
