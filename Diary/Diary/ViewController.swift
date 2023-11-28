f2fd252c9cb12f275f6d0d89bf4f28c8ece4153d
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-28 21:27:43 +0900

    ✨: TableViewCellをタップしたときに選択状態が解除される機能を追加

--- Diary/Diary/ViewController.swift
+++ Diary/Diary/ViewController.swift
@@ -62,4 +62,11 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
         return cell
     }
+
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
