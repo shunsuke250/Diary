a35e0e6c18cbf49bb80bdccde2c06e8941354f1d
shunsuke250 <soeyamashunsuke@gmail.com>
2023-12-01 21:39:25 +0900

    ✨: 新しい日記を追加するための画面を追加

---
+++ Diary/Diary/NewDiaryEntryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NewDiaryEntryViewController.swift
+//  Diary
+//
+//  Created by 副山俊輔 on 2023/12/01.
+//
+
+import UIKit
+
+class NewDiaryEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
