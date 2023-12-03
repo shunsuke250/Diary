bd1e6ee54094ced387271b155d4911bebe12958d
shunsuke250 <soeyamashunsuke@gmail.com>
2023-12-03 21:46:05 +0900

    👍: UI部品を定義

--- Diary/Diary/NewDiaryEntryViewController.swift
+++ Diary/Diary/NewDiaryEntryViewController.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class NewDiaryEntryViewController: UIViewController {
+    private var pageViewController: UIPageViewController!
+
+    private let diaryInputTextView: UITextView = {
+        $0.backgroundColor = .cyan
+        $0.text = "ここに日記を入力"
+        return $0
+    }(UITextView())
+
+    private let diaryDatePicker: UIDatePicker = {
+        return $0
+    }(UIDatePicker())
+
+    private let addDiaryButton: UIButton = {
+        return $0
+    }(UIButton())
 
     override func viewDidLoad() {
         super.viewDidLoad()
