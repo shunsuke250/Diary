21c7efe4a2db2ed13d540239f3e74f1df9ae981f
shunsuke250 <soeyamashunsuke@gmail.com>
2023-12-06 22:31:53 +0900

    ✨: CollectionViewCellにTextViewを表示する処理を追加

--- Diary/Diary/TextViewCollectionViewCell.swift
+++ Diary/Diary/TextViewCollectionViewCell.swift
@@ -8,5 +8,25 @@
 import UIKit
 
 class TextViewCollectionViewCell: UICollectionViewCell {
+    private let textView: UITextView = {
+        $0.text = "ここに入力"
+        return $0
+    }(UITextView())
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setup() {
+        addSubview(textView)
+
+        textView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
