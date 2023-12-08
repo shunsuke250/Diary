a3786d7143f7f60013580f3e3ffd2c492cc3175a
shunsuke250 <soeyamashunsuke@gmail.com>
2023-12-08 20:11:21 +0900

    👍: CompositionalLayoutを使用するように変更

--- Diary/Diary/NewDiaryEntryViewController.swift
+++ Diary/Diary/NewDiaryEntryViewController.swift
@@ -29,6 +29,39 @@ class NewDiaryEntryViewController: UIViewController {
             TextViewCollectionViewCell.self,
             forCellWithReuseIdentifier: "cell"
         )
+        view.backgroundColor = .white
+        setupConstrains()
+        collectionView.collectionViewLayout = createLayout()
+    }
+
+    private func setupConstrains() {
+        view.addSubview(collectionView)
+
+        collectionView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+        }
+    }
+
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(44))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+
+        return layout
+    }
+}
+
 extension NewDiaryEntryViewController: UICollectionViewDelegate {
 }
 
