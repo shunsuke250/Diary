e14920c9e3161ccea52b62a743fd53a1f57820cb
shunsuke250 <soeyamashunsuke@gmail.com>
2023-12-23 23:52:40 +0900

    👍: UI部品の名前を変更

--- Diary/Diary/DiaryTableViewCell.swift
+++ Diary/Diary/DiaryTableViewCell.swift
@@ -13,30 +13,30 @@ class DiaryTableViewCell: UITableViewCell {
         $0.alignment = .center
         $0.spacing = 0
         return $0
-    }(UIStackView(arrangedSubviews: [dateLabel, dayLabel]))
+    }(UIStackView(arrangedSubviews: [dayLabel, weekdayLabel]))
 
     private lazy var parentStackView: UIStackView = {
         $0.axis = .horizontal
         $0.alignment = .top
         $0.spacing = 10
         return $0
-    }(UIStackView(arrangedSubviews: [verticalStackView, diaryLabel]))
+    }(UIStackView(arrangedSubviews: [verticalStackView, diaryContentLabel]))
 
-    private let dateLabel: UILabel = {
+    private let dayLabel: UILabel = {
         $0.font = .systemFont(ofSize: 26.0, weight: .regular)
         $0.textColor = Color.black
         $0.text = "26"
         return $0
     }(UILabel())
 
-    private let dayLabel: UILabel = {
+    private let weekdayLabel: UILabel = {
         $0.font = .systemFont(ofSize: 12.0, weight: .regular)
         $0.textColor = Color.red
         $0.text = "日曜日"
         return $0
     }(UILabel())
 
-    private let diaryLabel: UILabel = {
+    private let diaryContentLabel: UILabel = {
         $0.font = .systemFont(ofSize: 16.0, weight: .regular)
         $0.numberOfLines = 5
         $0.text = "このように文章を\n改行して入力することができます。\nこのアプリはLifeBearの日記機能のUIを参考にして\n作成されています。"
