2a22cc74e611d4528dbc1781121ebd08fa5d80e2
shunsuke250 <soeyamashunsuke@gmail.com>
2024-09-11 23:38:32 +0900

    👍: 日記一覧画面のセルをリファクタリング

--- Diary/Diary/DiaryTableViewCell.swift
+++ Diary/Diary/DiaryTableViewCell.swift
@@ -8,43 +8,45 @@
 import UIKit
 
 class DiaryTableViewCell: UITableViewCell {
-    private lazy var verticalStackView: UIStackView = {
-        $0.axis = .vertical
-        $0.alignment = .center
-        $0.spacing = 0
-        return $0
-    }(UIStackView(arrangedSubviews: [dayLabel, weekdayLabel]))
-
-    private lazy var parentStackView: UIStackView = {
-        $0.axis = .horizontal
-        $0.alignment = .top
-        $0.spacing = 10
-        return $0
-    }(UIStackView(arrangedSubviews: [verticalStackView, diaryContentLabel]))
-
-    private let dayLabel: UILabel = {
+    private let dayLabel = UILabel().apply {
         $0.font = .systemFont(ofSize: 26.0, weight: .regular)
         $0.textColor = Color.black
         $0.text = "26"
-        return $0
-    }(UILabel())
+    }
 
-    private let weekdayLabel: UILabel = {
+    private let weekdayLabel = UILabel().apply {
         $0.font = .systemFont(ofSize: 12.0, weight: .regular)
         $0.textColor = Color.red
         $0.text = "日曜日"
-        return $0
-    }(UILabel())
+    }
 
-    private let diaryContentLabel: UILabel = {
+    private let diaryContentLabel = UILabel().apply {
         $0.font = .systemFont(ofSize: 16.0, weight: .regular)
         $0.numberOfLines = 5
         $0.text = "このように文章を\n改行して入力することができます。\nこのアプリはLifeBearの日記機能のUIを参考にして\n作成されています。"
-        return $0
-    }(UILabel())
+    }
+
+    private lazy var verticalStackView = UIStackView(arrangedSubviews: [
+        dayLabel,
+        weekdayLabel
+    ]).apply {
+        $0.axis = .vertical
+        $0.alignment = .center
+        $0.spacing = 0
+    }
+
+    private lazy var parentStackView = UIStackView(arrangedSubviews: [
+        verticalStackView,
+        diaryContentLabel
+    ]).apply {
+        $0.axis = .horizontal
+        $0.alignment = .top
+        $0.spacing = 10
+    }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
         setup()
     }
 
@@ -65,7 +67,12 @@ class DiaryTableViewCell: UITableViewCell {
         }
     }
 
-    func configure(day: String, weekday: Weekday, content: String) {
+    func configure(
+        date: Date,
+        day: String,
+        weekday: Weekday,
+        content: String
+    ) {
         dayLabel.attributedText = .init(
             string: day,
             attributes: [
@@ -75,7 +82,7 @@ class DiaryTableViewCell: UITableViewCell {
         weekdayLabel.attributedText = NSAttributedString(
             string: "\(weekday.displayName)曜日",
             attributes: [
-                .foregroundColor: weekday.color
+                .foregroundColor: weekday.color(date: date)
             ]
         )
         diaryContentLabel.text = content
